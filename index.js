088e488456eb457da80e69377d04751e9f75e33f
DenisIvanov26 <denis.ivanov26@gmail.com>
2023-08-22 11:58:25 -0500

    genesis

---
+++ index.js
@@ -0,0 +1,33 @@
+async function run() {
+  // [START storage_list_buckets]
+  // Imports the Google Cloud client library
+  const { Storage } = require("@google-cloud/storage");
+
+  // Creates a client
+  const storage = new Storage({
+    apiEndpoint: "http://localhost:8080",
+    projectId: "test",
+  });
+
+  // Lists all buckets in the current project
+  console.log("getting buckets")
+  const [buckets] = await storage.getBuckets();
+  console.log("Buckets:");
+  console.log(buckets);
+  buckets.forEach((bucket) => {
+    console.log(bucket.id);
+  });
+  // [END storage_list_buckets]
+
+  const [content] = await storage.bucket('sample-bucket')
+    .file('some_file.txt')
+    .download();
+  console.log("Contents:")
+  console.log(content.toString())
+}
+
+
+run().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
